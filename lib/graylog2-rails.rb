86ea9f7db94b95fea63d7f13523c4e97aaf47f95
Blake Hilscher <blake@hilscher.ca>
2013-11-04 17:17:56 -0500

    Refactor Graylog2Rails.configuration to cache yaml configuration

--- lib/graylog2-rails.rb
+++ lib/graylog2-rails.rb
@@ -1,11 +1,24 @@
 require "gelf"
-require "graylog2-rails/initializers"
 require "graylog2-rails/message"
 require "graylog2-rails/middleware"
-require "graylog2-rails/engine"
 require "graylog2-rails/notifier"
 require "graylog2-rails/version"
 
 module Graylog2Rails
-  mattr_accessor :gelf_config_options
+  
+  def self.configuration
+    # cached?
+    return @@configuration if @@configuration
+    # read
+    custom_config_file = Rails.root.join("config", "graylog2_rails.yml")
+    if custom_config_file.exist?
+      file_content = File.open(custom_config_file).read
+    else
+      puts "WARNING: Graylog2 config not found. Using default configuration options. Please run `rake graylog2_rails:install'"
+      file_content = File.open(File.expand_path(File.join("..", "..", "config", "graylog2_rails.yml"), __FILE__)).read
+    end
+    @@configuration = YAML.load(ERB.new(file_content).result)[Rails.env]
+    @@configuration
+  end
+  
 end
